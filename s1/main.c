ad6b028de643299476718755c8ce41b59240dbd4
Alexey <alex3122007@yandex.ru>
2024-01-31 21:41:37 +0300

    first commit

---
+++ s1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <locale.h>
+int c =1;
+
+int main(void){
+    int a = 3;
+    int b = 7;
+    printf("Hello World!\n");
+    printf("\nLet's \n\t go\n\t\t to walk \n");
+    printf("\n   *\n  ***\n *****\n*******\n HH HH\n ZZZZZ\n");
+
+
+    return 0;
+}
