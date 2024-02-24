d56697747c3e367c2713eac702219478217685c1
Alexey <alex3122007@yandex.ru>
2024-02-24 13:40:12 +0300

    homework4

--- s3/3.c
+++ s3/3.c
@@ -17,12 +17,10 @@ int main()
     int a, b, c;
     scanf("%d%d%d", &a, &b, &c);
 
-    if (a <= b && b <= c)
-    {
+    if (a <= b && b <= c){
         printf("Yes");
     }
-    else
-    {
+    else{
         printf("No");
     }
 
